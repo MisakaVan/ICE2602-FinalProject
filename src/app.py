988db05b65fc2a0ae41e30a6ffad548c0eb1f93b
Itareso <tom_zhu12345@163.com>
2023-12-17 21:42:27 +0800

    finish drafts about search page and result page

--- src/app.py
+++ src/app.py
@@ -1,36 +1,56 @@
 # SJTU EE208
 
+#from searchIndex import HtmlIndexSearcher
+import hashlib
+import random
+import string
+
 from flask import Flask, redirect, render_template, request, url_for
 from markupsafe import escape
-from searchIndex import HtmlIndexSearcher
-import hashlib
 
 app = Flask(__name__)
+
+class SearchResult:
+    def __init__(self, title, url="https://www.example.com", date="2023-12-17", content="news content here "*10):
+        self.title = title
+        self.url = url
+        self.date = date
+        self.content = content
+
+def getHotNews():
+    ret = []
+    for i in range(1, 7):
+        news = SearchResult("title" + str(i))
+        ret.append(news)
+    return ret
+
+def getResults(searchword):
+    ret = []
+    for i in range(1, 11):
+        news = SearchResult("title" + str(i))
+        ret.append(news)
+    return ret
 
 
 @app.route('/', methods=['POST', 'GET'])
 def search_form():
     if request.method == "POST":
-        ky = request.form['keyword']
-        return redirect(url_for('show_result', keyword=ky))
-    return render_template("search.html", hashcode=get_sha1("static/style.css"))
+        return redirect(url_for('show_result'))
+    hot_news = getHotNews()
+    left_column = hot_news[:3]
+    right_column = hot_news[3:]
+    return render_template("search.html", hashcode=get_sha1("static/search.css"), left_column=left_column, right_column=right_column)
 
 
 @app.route('/result', methods=['POST', 'GET'])
 def show_result():
     searchword = request.args.get('keyword')
-    search_result = searcher.query(searchword)
-    title, url, content = [], [], []
-    print("len=", len(search_result))
-    for result in search_result:
-        title.append(result.title)
-        url.append(result.url)
-        content.append("...add content here...")
-    lens = [i for i in range(len(title))]
+    results = getResults(searchword)
+    lens = [i for i in range(len(results))]
     if request.method == 'POST':
         keyword = request.form['keyword']
         return redirect(url_for('show_result', keyword=keyword))
-    return render_template("show_result.html", searchword=searchword, title=title, url=url, content=content, lens=lens)
+    return render_template("show_result.html", searchword=searchword, results=results, lens=lens, hashcode=get_sha1("static/result.css"))
 
 def get_sha1(filename):
     with open(filename, "rb") as f:
@@ -39,5 +59,5 @@ def get_sha1(filename):
     return sha1obj.hexdigest()
 
 if __name__ == '__main__':
-    searcher = HtmlIndexSearcher(store_dir="./lucene_index/")
+    #searcher = HtmlIndexSearcher(store_dir="./lucene_index/")
     app.run(debug=True, port=8080)
