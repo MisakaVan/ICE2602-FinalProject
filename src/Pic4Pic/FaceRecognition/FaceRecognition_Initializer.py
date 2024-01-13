4394e99f15f4efc8d556a20883293cb41241c162
Meloooo <2538358797@qq.com>
2024-01-13 13:28:48 +0800

    多人像搜索complete，见FaceRecognition_Ver3

--- src/Pic4Pic/FaceRecognition/FaceRecognition_Initializer.py
+++ src/Pic4Pic/FaceRecognition/FaceRecognition_Initializer.py
@@ -10,16 +10,14 @@ def build_index(image_database_path, index_path):
     for img_name in os.listdir(image_database_path):
         img_path = os.path.join(image_database_path, img_name)
         image = face_recognition.load_image_file(img_path)
-        if len( face_recognition.face_locations( image ) ) == 0:
-            continue
         encodings = face_recognition.face_encodings(image)
 
         if len(encodings) > 0:
-            index[img_name] = encodings[0]
+            index[img_name] = encodings
 
     np.save(index_path, index)
 
 image_database_path = 'playground\TEST_DataBase\Folder'
-index_path = 'playground\TEST_Vectors_FaceRecognition\IndexFile.npy'
+index_path = 'playground\TEST_Vectors_FaceRecognition_MultiFace\IndexFile.npy'
 
 build_index(image_database_path, index_path)
---
+++ src/Pic4Pic/FaceRecognition/FaceRecognition_Searcher_Ver3.py
@@ -0,0 +1,33 @@
+import face_recognition
+import numpy as np
+import time
+
+def LstMinEle(Lst):
+    TmpMin = 100
+    for i in Lst:
+        if i < TmpMin:
+            TmpMin = i
+    return TmpMin
+
+def find_similar_images(target_image_path, index_path, k=5):
+    IdxStart = time.time()
+    index = np.load(index_path, allow_pickle=True).item()
+    IdxEnd = time.time()
+    print( f"读取索引用时为：{IdxEnd-IdxStart}s" )
+    target_image = face_recognition.load_image_file(target_image_path)
+    target_encoding = face_recognition.face_encodings(target_image)[0]
+    EncodingEnd = time.time()
+    print( f"提取待查图片特征向量用时为：{EncodingEnd-IdxEnd}s" )
+    DictDistance = {}
+    for img_Name, Lstimg_Vec in index.items():
+        ThisDistance = face_recognition.face_distance( Lstimg_Vec, target_encoding )
+        DictDistance[img_Name] = LstMinEle(ThisDistance)
+    Lst_SortedDistance = sorted(DictDistance.items(), key=lambda x: x[1], reverse=False)
+    most_similar_images = Lst_SortedDistance[:k]
+    print( f"计算相似度用时为：{time.time()-EncodingEnd}s" )
+    return [img_name for img_name, _ in most_similar_images]
+
+target_image_path = r'playground\TEST_ToSearch\download.jpg'
+index_path = r'playground\TEST_Vectors_FaceRecognition_MultiFace\IndexFile.npy'
+similar_images = find_similar_images(target_image_path, index_path, k=5)
+print(similar_images)
